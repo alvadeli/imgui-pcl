1c2b534b1e530fada8f65452b5c53889a7b1a909
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-10-20 20:09:04 +0200

    Init

---
+++ src/imgui-pcl.cpp
@@ -0,0 +1,25 @@
+﻿// imgui-pcl.cpp : Defines the entry point for the application.
+//
+
+#include "imgui-pcl.h"
+
+int main()
+{
+    auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>(200, 1);
+    for (auto& point : *cloud)
+    {
+        point.x = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.y = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+    }
+
+    auto viewer = pcl::visualization::PCLVisualizer("3d Viewer");
+
+    viewer.addPointCloud(cloud);
+
+
+    while (!viewer.wasStopped())
+    {
+        viewer.spinOnce();
+    }
+}
---
+++ src/imgui-pcl.h
@@ -0,0 +1,10 @@
+﻿// imgui-pcl.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
+// TODO: Reference additional headers your program requires here.
